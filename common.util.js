42458ad3a87720b17d334aed2e62ca82017d3e24
Tim Wen <timwenchina@163.com>
2015-04-19 10:25:28 +0800

    tab功能代码修改

--- common.util.js
+++ common.util.js
@@ -1,5 +1,9 @@
 /**
  * Created by TimWen on 15/3/21.
+ * @author 温昊天
+ *
+ *
+ *
  */
 
 // 声明命名空间
@@ -27,7 +31,303 @@ COMMON.namespace = function(ns_string) {
 // 调用命名空间函数，设置新的命名空间
 COMMON.namespace('COMMON.UTIL');
 
-// tab切换功能的实现
+/**
+ * tab切换
+ * @param btnsObj
+ * @param contsObj
+ * @param options
+ */
+COMMON.UTIL.tab = function(btnsObj, contsObj, options) {
+    // 默认参数
+    var settings = {
+            time: 100,
+            index: 0,
+            event: 'hover',
+            activeClass: 'current'
+        },
+    // 新参数继承
+        settings = $.extend(settings, options || {});
+    // 变量声明
+    var _p = {},
+        btns = $(btnsObj),
+        btnCurrent,
+        conts,
+        contCurrent,
+        currentIndex,
+        overStatus,
+        timer,
+        trigger,
+        time = settings.time,
+        index = settings.index,
+        event = settings.event,
+        activeClass = settings.activeClass;
+    //
+    if(typeof contsObj !== 'string') {
+        conts = [], contCurrent = [];
+        $.each(contsObj, function () {
+            conts.push($(this.toString()));
+            contCurrent.push({});
+        });
+        contCurrent = [];
+    } else {
+        conts = $(contsObj);
+        contCurrent = {};
+    }
+    $.each(btns, function (i) {
+        if(event === 'hover') {
+            trigger = 'mouseenter';
+        } else if(event === 'click') {
+            trigger = 'click';
+        }
+        $(this).bind(trigger, {'i': i, 'o': btns[i]}, function(e) {
+            overStatus = true;
+            (function() {
+                var btn = e.data.o,
+                    index = e.data.i;
+                timer = setTimeout(function(o) {
+                    if (overStatus && currentIndex != index) {
+                        currentIndex = index;
+                        _p.activeBtn(btn);
+                        _p.activeConts(index);
+                        // _p.activeFun(index);
+                    }
+                }, time);
+            })();
+        }).bind('mouseleave', {"i": i}, function (e) {
+            clearTimeout(timer);
+            overStatus = false;
+        });
+    });
+    // 内部私有函数
+    _p.activeBtn = function(btnObj) {
+        $(btnCurrent).removeClass(activeClass);
+        btnCurrent = btnObj;
+        $(btnObj).addClass(activeClass);
+    };
+    _p.activeConts = function(num) {
+        if(typeof contsObj !== 'string') {
+            $.each(conts, function (i) {
+                $(contCurrent[i]).removeClass(activeClass);
+                contCurrent[i] = this[num];
+                $(this[num]).addClass(activeClass);
+            });
+        } else {
+            if(contCurrent) $(contCurrent).removeClass(activeClass);
+            contCurrent = conts[num];
+            $(conts[num]).addClass(activeClass);
+        }
+    };
+    /*
+     _p.activeFun = function (num) {
+     $.isFunction(fn) && fn(num);
+     };
+     */
+    // 私有函数调用
+    _p.activeBtn(btns[index]);
+    _p.activeConts(index);
+    // _p.activeFun(index);
+};
+
+
+// 图片轮播
+// 原命名为switch，由于switch是关键字，因此改成switchPics
+COMMON.UTIL.switchPics = function(option) {
+    var settings = {
+            currentTarget: '',
+            wrap: '',
+            autoplay : true,
+            minNum: 1,
+            time: 5000,
+            tab: '',
+            index: 0
+            /*,
+             prevCallback: function() {},
+             nextCallback: function() {}
+             */
+        },
+        settings = $.extend(settings, option || {});
+    var $currentTarget = $(settings.currentTarget),
+        $wrap = $(settings.wrap),
+        ul = $wrap.find('ul'),
+        li_len = ul.find('li').length,
+        li_w = ul.find('li').width(),
+        left = $currentTarget.find('.prev'),
+        right = $currentTarget.find('.next'),
+        tab = $(settings.tab),
+        timer = null,
+        currentIndex = settings.index;
+    tab.eq(currentIndex).addClass('current');
+    ul.css('width', li_w*li_len);
+    if(li_len > settings.minNum){
+        right.click(function(){
+            settings.nextCallback();
+            currentIndex++;
+            if(currentIndex == li_len) {
+                currentIndex = 0;
+            }
+            tab.removeClass('current').eq(currentIndex).addClass('current');
+            ul.stop().animate({
+                "left": -(li_w*currentIndex)
+            },{
+                "duration": 1000,
+                "easing": "easeOutExpo"
+            });
+        });
+        left.click(function() {
+            currentIndex--;
+            if(currentIndex < 0){
+                currentIndex = li_len - 1;
+            }
+            tab.removeClass('current').eq(currentIndex).addClass('current');
+            ul.stop().animate({
+                "left": -(li_w*currentIndex)
+            },{
+                "duration": 1000,
+                "easing": "easeOutExpo"
+            });
+            settings.prevCallback();
+        });
+        tab.click(function(){
+            currentIndex = $(this).index(settings.tab);
+            tab.removeClass('current').eq(currentIndex).addClass('current');
+            ul.stop().animate({
+                "left": -(li_w*currentIndex)
+            },{
+                "duration": 1000,
+                "easing": "easeOutExpo"
+            });
+        });
+        if (settings.autoplay) {
+            timer = setInterval(function() {
+                right.trigger("click");
+            }, settings.time);
+            $wrap.mouseenter(function() {
+                clearInterval(timer);
+            });
+            $wrap.mouseleave(function() {
+                clearInterval(timer);
+                timer = setInterval(function() {
+                    right.trigger("click");
+                }, settings.time);
+            });
+        }
+    }
+};
+
+
+
+
+
+
+
+
+
+// 闭包写法，有问题
+/*
+COMMON.UTIL.carousel = (function() {
+    var _e = {}, _p = {};
+    _e.t = function(a, b) {
+        var _p = {};
+
+        _p.f = function(a, b) {
+            console.log(a + b);
+        }
+        _p.f(a, b);
+    };
+
+    _e.tab = function(btnObj, contObj, params) {
+        // console.log(btnObj+contObj)
+        var _p = {};
+        // 映射函数（赋值使用）
+        _p.map = function(attribute, value) {
+            return (typeof params !== 'undefine' && typeof params[attribute] !== 'undefine') ? params[attribute] : value;
+        };
+        _p.activeBtn = function(btnObj) {
+            $(btnCurrent).removeClass(activeClass);
+            btnCurrent = btnObj;
+            $(btnObj).addClass(activeClass);
+        };
+        _p.activeConts = function(num) {
+            if(typeof contObj !== 'string') {
+                $.each(conts, function (i) {
+                    $(contCurrent[i]).removeClass(activeClass);
+                    contCurrent[i] = this[num];
+                    $(this[num]).addClass(activeClass);
+                });
+            } else {
+                if(contCurrent) {
+                    $(contCurrent).removeClass(activeClass);
+                }
+                contCurrent = conts[num];
+                $(conts[num]).addClass(activeClass);
+            }
+        };
+        _p.activeFun = function(num) {
+            if ($.isFunction(fn)) {
+                fn(num);
+            }
+        };
+
+        var btns = $(btnObj),
+            btnCurrent,
+            contCurrent,
+            conts,
+            currentIndex,
+            overStatus,
+            timer,
+            trigger,
+            time = _p.map('time', 100),
+            index = _p.map('index', 0),
+            event = _p.map('event', 'hover'),
+            activeClass = _p.map('activeClass', 'class');
+        if(typeof contObj !== 'string') {
+            conts = [], contCurrent = [];
+            $.each(contObj, function () {
+                conts.push($(this.toString()));
+                contCurrent.push({});
+            });
+            contCurrent = [];
+        } else {
+            conts = $(contObj);
+            contCurrent = {};
+        }
+        $.each(btns, function (i) {
+            // 判断标签触发的事件类型
+            if(event === 'hover') {
+                trigger = 'mouseenter';
+            } else if(event === 'click') {
+                trigger = 'click';
+            }
+            $(this).bind(trigger, {'i': i, 'o': btns[i]}, function(e) {
+                overStatus = true;
+                (function() {
+                    var btn = e.data.o,
+                        index = e.data.i;
+                    timer = setTimeout(function(o) {
+                        if(overStatus && currentIndex !== index) {
+                            currentIndex = index;
+                            _p.activeBtn(btn);
+                            _p.activeConts(index);
+                            _p.activeFun(index);
+                        }
+                    }, time);
+                })();
+            }).bind('mouseleave', {'i': i}, function(e) {
+                clearTimeout(timer);
+                overStatus = false;
+            });
+        });
+        _p.activeBtn(btns[index]);
+        _p.activeConts(index);
+        _p.activeFun(index);
+    };
+    return _e;
+})();
+*/
+
+
+
+/*
 COMMON.UTIL.tab = function(btnObj, conObj, params) {
     var btns = $(btnObj),
         btnCurrent,
@@ -107,91 +407,10 @@ COMMON.UTIL.tab = function(btnObj, conObj, params) {
     activeCons(index);
     activeFun(index);
 };
+*/
 
-// 图片轮播功能
-COMMON.UTIL.scrollPics = function(opt) {
-    var settings = {
-            currentTarget: '',
-            wrap: '',
-            autoplay : true,
-            minNum: 1,
-            time: 5000,
-            tab: '',
-            index: 0
-            /*,
-            prevCallback: function() {},
-            nextCallback: function() {}
-            */
-        },
-        opt = opt || {};
-    settings = $.extend(settings, opt);
-    var $currentTarget = $(settings.currentTarget),
-        $wrap = $(settings.wrap),
-        ul = $wrap.find('ul'),
-        li_len = ul.find('li').length,
-        li_w = ul.find('li').width(),
-        left = $currentTarget.find('.prev'),
-        right = $currentTarget.find('.next'),
-        tab = $(settings.tab),
-        timer = null,
-        currentIndex = settings.index;
-    tab.eq(currentIndex).addClass('current');
-    ul.css('width', li_w*li_len);
-    if(li_len > settings.minNum){
-        right.click(function(){
-            settings.nextCallback();
-            currentIndex++;
-            if(currentIndex == li_len) {
-                currentIndex = 0;
-            }
-            tab.removeClass('current').eq(currentIndex).addClass('current');
-            ul.stop().animate({
-                "left": -(li_w*currentIndex)
-            },{
-                "duration": 1000,
-                "easing": "easeOutExpo"
-            });
-        });
-        left.click(function() {
-            currentIndex--;
-            if(currentIndex < 0){
-                currentIndex = li_len - 1;
-            }
-            tab.removeClass('current').eq(currentIndex).addClass('current');
-            ul.stop().animate({
-                "left": -(li_w*currentIndex)
-            },{
-                "duration": 1000,
-                "easing": "easeOutExpo"
-            });
-            settings.prevCallback();
-        });
-        tab.click(function(){
-            currentIndex = $(this).index(settings.tab);
-            tab.removeClass('current').eq(currentIndex).addClass('current');
-            ul.stop().animate({
-                "left": -(li_w*currentIndex)
-            },{
-                "duration": 1000,
-                "easing": "easeOutExpo"
-            });
-        });
-        if (settings.autoplay) {
-            timer = setInterval(function() {
-                right.trigger("click");
-            }, settings.time);
-            $wrap.mouseenter(function() {
-                clearInterval(timer);
-            });
-            $wrap.mouseleave(function() {
-                clearInterval(timer);
-                timer = setInterval(function() {
-                    right.trigger("click");
-                }, settings.time);
-            });
-        }
-    }
-}
+
+
 
 
 
---
+++ js/page.js
@@ -0,0 +1,13 @@
+/**
+ * Created by TimWen on 15/4/18.
+ */
+/*
+(function(){
+    COMMON.UTIL.carousel.tab('.section-tab-btns a', '.tab-contsList');
+})();
+*/
+$(function() {
+    // COMMON.UTIL.carousel.tab('.section-tab-btns a', '.tab-contsList');
+    // COMMON.UTIL.carousel.t(1, 2);
+    COMMON.UTIL.tab('.section-tab-btns a', '.tab-contsList');
+});
